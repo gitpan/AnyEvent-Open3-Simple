f639d48804faa485fa5c10f08330fdf15c2a811f
Graham Ollis <plicease@cpan.org>
2013-02-05 13:55:10 +0000

    Import of PLICEASE/AnyEvent-Open3-Simple-0.64 from CPAN.
    gitpan-cpan-distribution: AnyEvent-Open3-Simple
    gitpan-cpan-version:      0.64
    gitpan-cpan-path:         PLICEASE/AnyEvent-Open3-Simple-0.64.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/Open3/Simple.pm
+++ lib/AnyEvent/Open3/Simple.pm
@@ -10,7 +10,7 @@ use Symbol qw( gensym );
 use AnyEvent::Open3::Simple::Process;
 
 # ABSTRACT: interface to open3 under AnyEvent
-our $VERSION = '0.63'; # VERSION
+our $VERSION = '0.64'; # VERSION
 
 
 sub new
@@ -118,7 +118,7 @@ AnyEvent::Open3::Simple - interface to open3 under AnyEvent
 
 =head1 VERSION
 
-version 0.63
+version 0.64
 
 =head1 SYNOPSIS
 
